49a276bec0bf77a58f27dcaafa187ead878252e0
Iqbal Khosti <iqbal.javed2004@icloud.com>
2024-08-07 12:15:51 -0400

    Added a static file for storing the data and created a bunch of html files for the well-progress of the data

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def index():
-    return "Hi baby"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5001, debug=True)
