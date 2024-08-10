9a4d0f3391d91e278a7735cf5ca3b0c24e8d5ef6
Iqbal Khosti <iqbal.javed2004@icloud.com>
2024-08-10 00:32:00 -0400

    Added a connection between the code and sqlalchmey and using the queries now we can easily grab the data from the database and display it on the webpage

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+from fin_database import engine
+from sqlalchemy import create_engine, text
 
 app = Flask(__name__, template_folder="templates")
 
+def load_customer_info():
+
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM user"))
+
+        client_data = [row._mapping for row in result.all()]
+
+        return client_data
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    info = load_customer_info()
+    return render_template("index.html", jobs=info)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
---
+++ fin_database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+
+#creating the connection between the mysql and code out here
+
+
+engine = create_engine("mysql+pymysql://sql3724833:bGeEVrbPrt@sql3.freemysqlhosting.net/sql3724833?charset=utf8mb4")
+
+# with engine.connect() as conn:
+#     result = conn.execute(text("SELECT * FROM user"))
+#     result_all = result.all()
+
+#     # here creating a dictionary of the sqlalchemy objects
+
+#     result_dicts = [row._mapping for row in result_all]
+    
+#     print(result_dicts)
