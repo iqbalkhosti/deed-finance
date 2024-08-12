966e001b7c37096fb112edd025b4fe6ed11bff9a
Iqbal Khosti <iqbal.javed2004@icloud.com>
2024-08-11 20:41:05 -0400

    new changes made and the page has a new page where the clicked applicants are displayed

--- app.py
+++ app.py
@@ -12,11 +12,34 @@ def load_customer_info():
         client_data = [row._mapping for row in result.all()]
 
         return client_data
+    
+def load_customer_specific_info(id):
+    
+    with engine.connect() as conn:
+        customer = conn.execute(text("SELECT * FROM user WHERE id = :val"), {"val": id})
+        
+        # customer = conn.execute(text("SELECT * FROM user WHERE id = :val"), val=id)
+        
+        rows= customer.all()
+
+        rows_dict = [row._mapping for row in rows]
+
+        if len(rows)==0:
+            return None
+        else:
+            return rows_dict[0]
 
 @app.route("/")
 def index():
     info = load_customer_info()
     return render_template("index.html", jobs=info)
+
+@app.route("/customer/<id>")
+def load_info_by_id(id):
+    
+    customer = load_customer_specific_info(id)
+
+    return render_template("jobitem.html", user=customer)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
